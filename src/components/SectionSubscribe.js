476dcb3fdead92d10e396bfe95db53da705cb9b7
dylangangat <dylan.gangat@gmail.com>
2022-03-10 19:58:03 +0200

    Complete responsive subscribe section and add form validation to the email

---
+++ src/components/SectionSubscribe.js
@@ -0,0 +1,77 @@
+// Styles
+import { useState } from "react";
+import "./SectionSubscribe.scss";
+
+export default function SectionSubscribe() {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  // Regex for the email test
+  const emailValidation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  const resetValidation = () => {
+    setSuccess("");
+    setError("");
+    setEmail("");
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    // If there is NOT a email state value stored this function with return.
+    if (!email) return;
+
+    // Validation of the email
+    if (emailValidation.test(email)) {
+      setSuccess(true);
+    } else {
+      setError(true);
+    }
+
+    //Reset the entire validation after 2 secs
+    setTimeout(() => {
+      resetValidation();
+    }, 1000);
+  };
+  return (
+    <section className="subscribe">
+      <div className="container">
+        <h2 className="center">Subscribe to get the best deals</h2>
+
+        {/* Removed the default validation so I could add mine */}
+
+        <form className="email-form spacer" onSubmit={handleSubmit} noValidate>
+          <div className="input">
+            <label htmlFor="email" className="visually-hidden">
+              Enter yor email:
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Enter your email"
+              //   Updates state whenever you type in input
+              onChange={e => setEmail(e.target.value)}
+              // sets the value to current state
+              value={email}
+            />
+            {error && (
+              <p className={error ? "error" : ""}>
+                Please enter a valid email address!
+              </p>
+            )}
+            {success && (
+              <p className={success ? "success" : ""}>
+                Thank you for subscribing!
+              </p>
+            )}
+          </div>
+
+          <button className="btn" type="submit">
+            Subscribe
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+}
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -6,6 +6,7 @@ import Hero from "../../components/Hero";
 import SectionTrust from "../../components/SectionTrust";
 import SectionReview from "../../components/SectionReview";
 import SectionProperty from "../../components/SectionProperty";
+import SectionSubscribe from "../../components/SectionSubscribe";
 
 export default function Home() {
   // Main section where you should pass props from and remember you should NOT PROP DRILL!
@@ -18,6 +19,8 @@ export default function Home() {
       <SectionReview />
 
       <SectionProperty />
+
+      <SectionSubscribe />
     </>
   );
 }
